73f8773f52a15d61a5bd9ab70dca96f81fe95f35
estenpro <estenhl@student.matnat.uio.no>
2016-05-10 20:46:56 +0200

    Removed image files and indexes

--- examples/hla-triple.sh
+++ examples/hla-triple.sh
@@ -1,3 +1,3 @@
 java -jar ../target/graph-genome.jar index --index=hla.index -if=../data/mhc_A3105/primary.fasta --png=hla
-java -jar ../target/graph-genome.jar align --index=hla.index -af=../data/mhc_A3105/alt_loci_1.fasta --merge=true -em=3 -heur=true --png=hla
-java -jar ../target/graph-genome.jar align --index=hla.index -af=../data/mhc_A3105/alt_loci_2.fasta --merge=true -em=4 -heur=true --png=hla
+java -jar ../target/graph-genome.jar align --index=hla.index -af=../data/mhc_A3105/alt_loci_1.fasta --merge=true -em=3 -heur=true
+java -jar ../target/graph-genome.jar align --index=hla.index -af=../data/mhc_A3105/alt_loci_2.fasta --merge=true -em=4 -heur=true --png=hla-triple
